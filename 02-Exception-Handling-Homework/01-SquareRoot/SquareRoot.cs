43deb0145311abde41399324ef1bea2e7c183eda
tockata <anatoly.angelov@gmail.com>
2015-01-24 10:51:58 +0200

    added 02-Exception-Handling-Homework

---
+++ 02-Exception-Handling-Homework/01-SquareRoot/SquareRoot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _01_SquareRoot
+{
+    public class SquareRoot
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            try
+            {
+                int number = int.Parse(input);
+                if (number < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                double squareRoot = Math.Sqrt(number);
+                Console.WriteLine(squareRoot);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!");
+            }
+        }
+    }
+}
---
+++ 02-Exception-Handling-Homework/02-EnterNumbers/EnterNumbers.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _02_EnterNumbers
+{
+    public class EnterNumbers
+    {
+        static void Main()
+        {
+            int start = 1;
+            int end = 100;
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                bool validNumber = false;
+                do
+                {
+                    try
+                    {
+                        int tempNumber = ReadNumber(start, end);
+                        if (i > 0 && tempNumber <= numbers[i - 1])
+                        {
+                            throw new ArgumentOutOfRangeException();
+                        }
+                        numbers[i] = tempNumber;
+                        validNumber = true;
+                    }
+                    catch (ArgumentOutOfRangeException)
+                    {
+                        Console.WriteLine("Number must be greater than {0}", numbers[i - 1]);
+                    }
+                } while (!validNumber);
+            }
+
+            printNumbers(numbers);
+        }
+
+        private static void printNumbers(int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.Write(number + " ");
+            }
+        }
+
+        public static int ReadNumber(int start, int end)
+        {
+            while (true)
+            {
+                Console.Write("Enter integer number: ");
+                string input = Console.ReadLine();
+                try
+                {
+                    int num = int.Parse(input);
+                    if (num < start || num > end)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    return num;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("You entered invalid number!");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Number must be between {0} and {1}", start, end);
+                }
+            }
+        }
+    }
+}
